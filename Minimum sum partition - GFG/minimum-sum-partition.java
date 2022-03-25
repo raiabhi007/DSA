0808975de6cf72695dd9f5831f6d9eecd431b3d2
raiabhi007 <63101700+raiabhi007@users.noreply.github.com>
2022-03-25 10:26:37 +0530

    Added solution - LeetHub

---
+++ Minimum sum partition - GFG/minimum-sum-partition.java
@@ -0,0 +1,59 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+class GfG
+{
+    public static void main(String args[])
+        {
+            Scanner sc = new Scanner(System.in);
+            int t = sc.nextInt();
+            while(t-->0)
+                {
+                    int n = sc.nextInt();
+                    int A[] = new int[n];
+                    for(int i = 0;i<n;i++)
+                        A[i] = sc.nextInt();
+                    Solution ob = new Solution();
+                    System.out.println(ob.minDifference(A,n));
+                }
+        }
+}    // } Driver Code Ends
+class Solution
+{
+	public int minDifference(int arr[], int n){ 
+	  int range = 0;
+	  for(int i=0;i<n;i++){
+	      range+=arr[i];
+	  }
+	  boolean[][] dp = new boolean[n+1][range+1];
+	  for(int i=0;i<=n;i++){
+	      for(int j=0;j<=range;j++){
+	          if(i==0){
+	              if(j==0)
+	              dp[i][j] = true;
+	              else
+	              dp[i][j] = false;
+	          }
+	          else if(j<arr[i-1])
+	          dp[i][j] = dp[i-1][j];
+	          else{
+	              if(dp[i-1][j-arr[i-1]]==true||dp[i-1][j]==true)
+	              dp[i][j] = true;
+	              else
+	              dp[i][j] = false;
+	          }
+	      }
+	  }
+	  ArrayList<Integer> list = new ArrayList<>();
+	  for(int i=0;i<=range/2;i++){
+	      if(dp[n][i]==true)
+	      list.add(i);
+	  }
+	  int ans = Integer.MAX_VALUE;
+	  for(int i=0;i<list.size();i++)
+	  ans = Math.min(ans,range-2*list.get(i));
+	  return ans;
+	} 
+}
