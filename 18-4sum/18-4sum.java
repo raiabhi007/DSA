f553dad259232e2a3e1c106098912df0ae5837f8
raiabhi007 <63101700+raiabhi007@users.noreply.github.com>
2022-04-11 10:06:06 +0530

    Time: 29 ms (48.87%), Space: 45 MB (46.04%) - LeetHub

---
+++ 18-4sum/18-4sum.java
@@ -0,0 +1,32 @@
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        List<List<Integer>> list = new ArrayList<>();
+        for(int i=0;i<n-3;i++){
+            if(i==0||(i>0&&nums[i]!=nums[i-1])){
+                for(int j=i+1;j<n-2;j++){
+                    if(j==i+1||(j>i+1&&nums[j]!=nums[j-1])){
+                    int k = j+1;
+                    int l = n-1;
+                    while(k<l){                        
+                        int sum= nums[i]+nums[j]+nums[k]+nums[l];
+                        if(sum==target){
+                            list.add(Arrays.asList(nums[i],nums[j],nums[k],nums[l]));
+                            while(k<l&&nums[k]==nums[k+1]) k++;
+                            while(k<l&&nums[l]==nums[l-1]) l--;
+                            k++;
+                            l--;
+                          }
+                        else if(sum>target)
+                            l--;
+                        else
+                            k++;
+                        }
+                    }
+                }
+            }
+        }
+        return list;
+    }
+}
