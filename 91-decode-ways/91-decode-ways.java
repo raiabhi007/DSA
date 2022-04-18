8fbeb2bdc5697e27bedd410025f33fe390dd54d6
raiabhi007 <63101700+raiabhi007@users.noreply.github.com>
2022-04-18 11:16:58 +0530

    Time: 1 ms (94.89%), Space: 40.4 MB (93.70%) - LeetHub

--- 91-decode-ways/91-decode-ways.java
+++ 91-decode-ways/91-decode-ways.java
@@ -1,20 +1,20 @@
 class Solution {
     public int numDecodings(String s) {
-        if(s==null||s.length()==0)
-            return 0;
+        Integer dp[] = new Integer[s.length()+1];
+        return s.length()=='0'?0:helper(0,s,dp);
+    }
+    public int helper(int p,String s,Integer[] dp){
         int n = s.length();
-        int dp[] = new int[n+1];
-        dp[0] = 1;
-        dp[1] = s.charAt(0)!='0'?1:0;
-        
-        for(int i=2;i<=n;i++){
-            int first = Integer.valueOf(s.substring(i-1,i));
-            int second = Integer.valueOf(s.substring(i-2,i));
-            if(first>=1&&first<=9)
-                dp[i]+=dp[i-1];
-            if(second>=10&&second<=26)
-                dp[i]+=dp[i-2];
-        }
-        return dp[n];
+        if(p==n)
+            return 1;
+        if(s.charAt(p)=='0')
+            return 0;
+        if(dp[p]!=null)
+            return dp[p];
+        int res=0;
+            res+=helper(p+1,s,dp);
+        if(p<n-1&&(s.charAt(p)=='1'||s.charAt(p)=='2'&&s.charAt(p+1)<'7'))
+            res+=helper(p+2,s,dp);
+        return dp[p]=res;
     }
 }
