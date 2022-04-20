d5ae3752de60cb1489d7c33c9a7e5cb6ad208ef8
raiabhi007 <63101700+raiabhi007@users.noreply.github.com>
2022-04-20 09:46:13 +0530

    Time: 33 ms (37.82%), Space: 44.4 MB (74.24%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -1,27 +1,25 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
         int dp[] = new int[amount+1];
-         Arrays.fill(dp,-1);
-         dp[0] = 0;
-        int ans = helper(coins,amount,dp);
-       
-        return (ans!=Integer.MAX_VALUE)?ans:-1;
+        Arrays.fill(dp,-1);
+        dp[0] = 0;
+        int res = coinChangeHelper(coins,amount,dp);
+        return res==Integer.MAX_VALUE?-1:res;
     }
-    public int helper(int[] coins,int amount,int dp[]){
-        if(dp[amount]!=-1)
-            return dp[amount];
-        if(amount==0)
-            return 0;
-        int ans = Integer.MAX_VALUE;
-        for(int i=0;i<coins.length;i++){
-            if(amount>=coins[i]){
-                int temp = helper(coins,amount-coins[i],dp);
-                ans = Math.min(ans,temp);
-            }
-        }
-        if(ans==Integer.MAX_VALUE)
-            return dp[amount]=ans;
+    public int coinChangeHelper(int[] coins,int amount,int[] dp){
+         if(dp[amount]!=-1)
+           return dp[amount];
+        
+        int res = Integer.MAX_VALUE;
+       for(int i=0;i<coins.length;i++){
+           if(amount-coins[i]>=0){
+           int temp = coinChangeHelper(coins,amount-coins[i],dp);
+           res = Math.min(res,temp);
+           }
+       }
+        if(res==Integer.MAX_VALUE)
+        return dp[amount]=res;
         else
-            return dp[amount]=ans+1;
+        return dp[amount] = res+1;
     }
 }
