030a37174f2fc122cd4d0ecb433603463f785484
raiabhi007 <63101700+raiabhi007@users.noreply.github.com>
2022-05-01 16:20:47 +0530

    Time: 1 ms (67.12%), Space: 42 MB (40.32%) - LeetHub

--- 856-score-of-parentheses/856-score-of-parentheses.java
+++ 856-score-of-parentheses/856-score-of-parentheses.java
@@ -1,27 +1,27 @@
 class Solution {
     public int scoreOfParentheses(String s) {
-        Stack<Integer> st = new Stack<>();
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i)=='(')
-                st.push(-1);
-            else{
-                if(st.peek()==-1){
-                    st.pop();
-                    st.push(1);
-                }else{
-                    int val = 0;
-                    while(st.peek()!=-1){
-                        val+=st.pop();
-                    }
-                    st.pop();
-                    st.push(2*val);
-                }
-            }
-        }
-        int val =0;
-        while(st.size()>0){
-            val+=st.pop();
-        }
-        return val;
+       Stack<Integer> st = new Stack<>();
+        int n = s.length();
+       for(int i=0;i<n;i++){
+           if(s.charAt(i)=='('){
+               st.push(-1);
+           }else{
+               if(st.peek()==-1){
+                   st.pop();
+                   st.push(1);
+               }
+               else{
+                   int temp = 0;
+                   while(st.peek()!=-1)
+                       temp+=st.pop();
+                   st.pop();
+                   st.push(2*temp);
+               }
+           }
+       }
+        int ans = 0;
+        while(st.size()>0)
+            ans+=st.pop();
+        return ans;
     }
 }
