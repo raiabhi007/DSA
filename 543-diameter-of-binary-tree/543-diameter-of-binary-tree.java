324fe6fba73ef9466fe8bcf19b0c29ac6d96b71c
raiabhi007 <63101700+raiabhi007@users.noreply.github.com>
2022-06-09 11:59:19 +0530

    Time: 2 ms (19.07%), Space: 43.7 MB (24.79%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.java
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.java
@@ -1,36 +1,29 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+public class Pair<T,V>{
+    public T first;
+    public T second;
+}
 class Solution {
     public int diameterOfBinaryTree(TreeNode root) {
-        if(root==null)
-            return 0;
-        int left = diameterOfBinaryTree(root.left);
-        int right = diameterOfBinaryTree(root.right);
-        
-        int leftHeight = height(root.left);
-        int rightHeight = height(root.right);
-        
-        return Math.max(leftHeight+rightHeight,Math.max(left,right));
+        Pair<Integer,Integer> ans = heightDiameter(root);
+        return ans.second;
     }
-    public int height(TreeNode node){
-        if(node==null)
-            return 0;
-        int left = height(node.left);
-        int right = height(node.right);
-        
-        return Math.max(left,right)+1;
+    public Pair<Integer,Integer> heightDiameter(TreeNode root){
+        if(root==null){
+            Pair<Integer,Integer> output = new Pair<>();
+            output.first = 0;
+            output.second = 0;
+            return output;
+        }
+        Pair<Integer,Integer> lo = heightDiameter(root.left);
+        Pair<Integer,Integer> ro = heightDiameter(root.right);
+        int height = 1+Math.max(lo.first,ro.first);
+        int option1 = lo.first+ro.first;
+        int option2 = lo.second;
+        int option3 = ro.second;
+        int diamter1 = Math.max(option1,Math.max(option2,option3));
+        Pair<Integer,Integer> output = new Pair<>();
+        output.first = height;
+        output.second = diamter1;
+        return output;
     }
 }
