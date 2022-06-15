d5d1d05e0b12e32f28f41d28329a6354141cac28
raiabhi007 <63101700+raiabhi007@users.noreply.github.com>
2022-06-15 18:08:58 +0530

    Time: 1 ms (98.42%), Space: 44.7 MB (8.36%) - LeetHub

--- 889-construct-binary-tree-from-preorder-and-postorder-traversal/889-construct-binary-tree-from-preorder-and-postorder-traversal.java
+++ 889-construct-binary-tree-from-preorder-and-postorder-traversal/889-construct-binary-tree-from-preorder-and-postorder-traversal.java
@@ -14,14 +14,34 @@
  * }
  */
 class Solution {
-    int preIndex = 0,posIndex = 0;
     public TreeNode constructFromPrePost(int[] pre, int[] post) {
-        TreeNode root = new TreeNode(pre[preIndex++]);
-        if (root.val != post[posIndex])
-            root.left = constructFromPrePost(pre, post);
-        if (root.val != post[posIndex])
-            root.right = constructFromPrePost(pre, post);
-        posIndex++;
+        return buildTree(pre,post,0,pre.length-1,0,post.length-1);
+    }
+    public TreeNode buildTree(int[] pre,int[] post,int preS,int preE,int postS,int postE){
+        if(preS>preE)
+            return null;
+        TreeNode root = new TreeNode(pre[preS]);
+        if(preS==preE)
+            return root;
+        int idx = postS;
+        for(int i=postS;i<=postE;i++){
+            if(post[i]==pre[preS+1]){
+                idx = i;
+                break;
+            }
+        }
+        int tnel = idx-postS+1;
+        int leftPreS = preS+1;
+        int leftPreE = preS+tnel;
+        int leftPostS = postS;
+        int leftPostE = idx;
+        int rightPreS = leftPreE+1;
+        int rightPreE = preE;
+        int rightPostS = leftPostE+1;
+        int rightPostE = postE-1;
+        
+        root.left = buildTree(pre,post,leftPreS,leftPreE,leftPostS,leftPostE);
+        root.right = buildTree(pre,post,rightPreS,rightPreE,rightPostS,rightPostE);
         return root;
     }
 }
