eb2761405b115a62f047b6774841b2a485f8c43e
raiabhi007 <63101700+raiabhi007@users.noreply.github.com>
2022-07-20 23:13:04 +0530

    Time: 14 ms (18.47%), Space: 49.1 MB (5.45%) - LeetHub

--- 51-n-queens/51-n-queens.java
+++ 51-n-queens/51-n-queens.java
@@ -6,18 +6,17 @@ class Solution {
         boolean d1[] = new boolean[2*n-1];
         boolean d2[] = new boolean[2*n-1];
         List<String> list = new ArrayList<>();
-        nQueen(board,col,d1,d2,0,list);
+        nQueen(col,d1,d2,0,list);
         return ans;
     }
-    public void nQueen(boolean[][] board,boolean[] col,boolean[] d1,boolean[] d2,int i,List<String> list){
-        int n = board.length;
+    public void nQueen(boolean[] col,boolean[] d1,boolean[] d2,int i,List<String> list){
+        int n = col.length;
         if(i==n){
             ans.add(new ArrayList<>(list));
             return;
         }
-        for(int j=0;j<board.length;j++){
+        for(int j=0;j<n;j++){
             if(col[j]==false&&d1[i+j]==false&&d2[i-j+n-1]==false){
-                board[i][j] = true;
                 col[j] = true;
                 d1[i+j] =true;
                 d2[i-j+n-1] = true;
@@ -29,9 +28,8 @@ class Solution {
                         s = s+".";
                 }
                 list.add(s);
-                nQueen(board,col,d1,d2,i+1,list);
+                nQueen(col,d1,d2,i+1,list);
                     list.remove(list.size()-1);
-                 board[i][j] = false;
                 col[j] = false;
                 d1[i+j] = false;
                 d2[i-j+n-1] = false;
