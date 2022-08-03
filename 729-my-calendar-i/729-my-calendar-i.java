361b73b42d91a6fa184132ecae6cad904ee11344
raiabhi007 <63101700+raiabhi007@users.noreply.github.com>
2022-08-03 22:49:54 +0530

    Time: 40 ms (50.82%), Space: 54.6 MB (44.49%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.java
+++ 729-my-calendar-i/729-my-calendar-i.java
@@ -1,11 +1,16 @@
 class MyCalendar {
-    private List<int[]> books = new ArrayList<>();
-    public boolean book(int start, int end) {
-        for (int[] b : books)
-            if (Math.max(b[0], start) < Math.min(b[1], end)) return false;
-        books.add(new int[]{ start, end });
-        return true;
+  public boolean book(int start, int end) {
+    Integer low = timeline.lowerKey(end);
+
+    if (low == null || timeline.get(low) <= start) {
+      timeline.put(start, end);
+      return true;
     }
+
+    return false;
+  }
+
+  private TreeMap<Integer, Integer> timeline = new TreeMap<>();
 }
 /**
  * Your MyCalendar object will be instantiated and called as such:
