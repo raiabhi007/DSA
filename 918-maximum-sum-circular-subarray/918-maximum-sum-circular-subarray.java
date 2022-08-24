98f28fcf154213d710730fccd91539b36e6c0108
raiabhi007 <63101700+raiabhi007@users.noreply.github.com>
2022-08-24 22:49:09 +0530

    Time: 6 ms (52.98%), Space: 61.9 MB (79.10%) - LeetHub

--- 918-maximum-sum-circular-subarray/918-maximum-sum-circular-subarray.java
+++ 918-maximum-sum-circular-subarray/918-maximum-sum-circular-subarray.java
@@ -1,28 +1,24 @@
 class Solution {
     public int maxSubarraySumCircular(int[] nums) {
-        int maxStraightSum = Integer.MIN_VALUE;
-        int minStraightSum = Integer.MAX_VALUE;
-        int arraySum = 0;
-        
-        int tempMaxSum = 0;
-        int tempMinSum = 0;
+        int max_sum = Integer.MIN_VALUE;
+        int temp_max = 0;
+        int min_sum = Integer.MAX_VALUE;
+        int temp_min = 0;
+        int arr_sum = 0;
         
         for(int i=0;i<nums.length;i++){
-            arraySum+=nums[i];
-            tempMaxSum+=nums[i];
-            if(tempMaxSum>maxStraightSum)
-                maxStraightSum = tempMaxSum;
-            if(tempMaxSum<0)
-                tempMaxSum = 0;
-            
-            tempMinSum+=nums[i];
-            if(tempMinSum<minStraightSum)
-                minStraightSum = tempMinSum;
-            if(tempMinSum>0)
-                tempMinSum = 0;
+            temp_max+=nums[i];
+            max_sum = Math.max(temp_max,max_sum);
+            if(temp_max<0)
+                temp_max = 0;
+            temp_min+=nums[i];
+            min_sum = Math.min(min_sum,temp_min);
+            if(temp_min>0)
+                temp_min = 0;
+            arr_sum+=nums[i];
         }
-        if(arraySum==minStraightSum)
-            return maxStraightSum;
-        return Math.max(maxStraightSum,arraySum-minStraightSum);
+        if(arr_sum==min_sum)
+            return max_sum;
+        return Math.max(max_sum,arr_sum-min_sum);
     }
 }
