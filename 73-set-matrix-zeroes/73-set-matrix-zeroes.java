eab5183f9aa1ceea518253fe589474ecda28f60a
raiabhi007 <63101700+raiabhi007@users.noreply.github.com>
2022-09-13 08:53:37 +0530

    Time: 3 ms (26.11%), Space: 54.1 MB (57.21%) - LeetHub

---
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -0,0 +1,22 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int dummy1[] = new int[matrix.length];
+        int dummy2[] = new int[matrix[0].length];
+        Arrays.fill(dummy1,-1);
+        Arrays.fill(dummy2,-1);
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                if(matrix[i][j]==0){
+                    dummy1[i] = 0;
+                    dummy2[j] = 0;
+                }
+            }
+        }
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                if(dummy1[i]==0||dummy2[j]==0)
+                    matrix[i][j] = 0;
+            }
+        }
+    }
+}
