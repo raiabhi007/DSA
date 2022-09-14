e6c69775b0e03eaa0b0cecb920c7c2d0ed9b16aa
raiabhi007 <63101700+raiabhi007@users.noreply.github.com>
2022-09-14 08:16:12 +0530

    Time: 3 ms (25.64%), Space: 72.6 MB (84.58%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.java
+++ 53-maximum-subarray/53-maximum-subarray.java
@@ -4,7 +4,7 @@ class Solution {
         int cur = 0;
         for(int i=0;i<nums.length;i++){
             cur+=nums[i];
-            ans = Math.max(ans,cur);
+            ans = Math.max(cur,ans);
             if(cur<0)
                 cur = 0;
         }
