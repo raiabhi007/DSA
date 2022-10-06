2d807760cba86d1a6c4c58c806fb684a73d8272d
raiabhi007 <63101700+raiabhi007@users.noreply.github.com>
2022-10-06 11:13:27 +0530

    Time: 384 ms (39.13%), Space: 215.1 MB (74.01%) - LeetHub

--- 981-time-based-key-value-store/981-time-based-key-value-store.java
+++ 981-time-based-key-value-store/981-time-based-key-value-store.java
@@ -1,29 +1,41 @@
+class Data {
+    String val;
+    int time;
+    Data(String val, int time) {
+        this.val = val;
+        this.time = time;
+    }
+}
 class TimeMap {
 
-     private Map<String,TreeMap<Integer,String>> map;
-
     /** Initialize your data structure here. */
+    Map<String, List<Data>> map;
     public TimeMap() {
-        map = new HashMap<>();
+        map = new HashMap<String, List<Data>>();
     }
-
+    
     public void set(String key, String value, int timestamp) {
-        if(!map.containsKey(key)) {
-            map.put(key,new TreeMap<>());
-        }
-        map.get(key).put(timestamp,value);
+        if (!map.containsKey(key)) map.put(key, new ArrayList<Data>());
+        map.get(key).add(new Data(value, timestamp));
     }
-
+    
     public String get(String key, int timestamp) {
-        TreeMap<Integer,String> treeMap = map.get(key);
-        if(treeMap==null) {
-            return "";
+        if (!map.containsKey(key)) return "";
+        return binarySearch(map.get(key), timestamp);
+    }
+    
+    protected String binarySearch(List<Data> list, int time) {
+        int low = 0, high = list.size() - 1;
+        while (low < high) {
+            int mid = (low + high) >> 1;
+            if (list.get(mid).time == time) return list.get(mid).val;
+            if (list.get(mid).time < time) {
+                if (list.get(mid+1).time > time) return list.get(mid).val;
+                low = mid + 1;
+            }
+            else high = mid -1;
         }
-        Integer floor = treeMap.floorKey(timestamp);
-        if(floor==null) {
-            return "";
-        }
-        return treeMap.get(floor);
+        return list.get(low).time <= time ? list.get(low).val : "";
     }
 }
 
