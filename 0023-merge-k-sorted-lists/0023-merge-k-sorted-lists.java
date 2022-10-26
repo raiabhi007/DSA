cd6f3c184f1c574d9dc981234fca5fcc2b1b90bf
raiabhi007 <63101700+raiabhi007@users.noreply.github.com>
2022-10-26 16:41:40 +0530

    Time: 3 ms (92.32%), Space: 47.9 MB (21.79%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -10,22 +10,39 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-         Queue<Integer> minHeap = new PriorityQueue<>();
+         int size = lists.length;
+        int interval = 1;
 
-        for (ListNode nodes : lists) {
-            ListNode current = nodes;
-            while (current != null) {
-                minHeap.add(current.val);
-                current = current.next;
+        while (interval < size) {
+            for (int i = 0; i < size - interval; i += 2 * interval) {
+                lists[i] = merge(lists[i], lists[i + interval]);
             }
+
+            interval *= 2;
         }
 
+        return size > 0 ? lists[0] : null;
+    }
+     private ListNode merge(ListNode l1, ListNode l2) {
         ListNode dummy = new ListNode(0);
-        ListNode current = dummy;
+        ListNode curr = dummy;
 
-        while (!minHeap.isEmpty()) {
-            current.next = new ListNode(minHeap.poll());
-            current = current.next;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                curr.next = l1;
+                l1 = l1.next;
+            } else {
+                curr.next = l2;
+                l2 = l2.next;
+            }
+
+            curr = curr.next;
+        }
+
+        if (l1 != null) {
+            curr.next = l1;
+        } else {
+            curr.next = l2;
         }
 
         return dummy.next;
