2a3afd7a3507cb3d5c9c5a05a6136d878c3f215e
raiabhi007 <63101700+raiabhi007@users.noreply.github.com>
2022-11-08 13:01:55 +0530

    Time: 13 ms (9.83%), Space: 54.2 MB (13.32%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.java
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.java
@@ -1,18 +1,12 @@
 class Solution {
-    public boolean check(int start,int V, ArrayList<ArrayList<Integer>> adj,int color[]){
-        Queue<Integer> q = new LinkedList<>();
-        q.add(start);
-        color[start] = 0;
-        while(!q.isEmpty()){
-            int node = q.peek();
-            q.remove();
-            for(int it:adj.get(node)){
-                if(color[it]==-1){
-                    color[it] = 1-color[node];
-                    q.add(it);
-                }else if(color[it]==color[node])
+    public boolean check(int start,int prevColor, ArrayList<ArrayList<Integer>> adj,int color[]){
+        color[start] = 1-prevColor;
+        for(int it:adj.get(start)){
+            if(color[it]==-1){
+                if(check(it,color[start],adj,color)==false)
                     return false;
-            }
+            }else if(color[it]==color[start])
+                return false;
         }
         return true;
     }
@@ -29,7 +23,7 @@ class Solution {
         Arrays.fill(color,-1);
         for(int i=0;i<V;i++){
             if(color[i]==-1){
-                if(check(i,V,adj,color)==false)
+                if(check(i,1,adj,color)==false)
                     return false;
             }
         }
