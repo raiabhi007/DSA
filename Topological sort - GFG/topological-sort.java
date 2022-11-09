a09ec89145b8e76715955736684a18475a7dd433
raiabhi007 <63101700+raiabhi007@users.noreply.github.com>
2022-11-09 13:49:37 +0530

    Added solution - LeetHub

--- Topological sort - GFG/topological-sort.java
+++ Topological sort - GFG/topological-sort.java
@@ -60,29 +60,29 @@ class Main {
 
 class Solution
 {
-    //Function to return list containing vertices in Topological order.
-    static void dfs(int node,ArrayList<ArrayList<Integer>> adj,int[] vis,Stack<Integer> st){
-        vis[node] = 1;
-        for(int it:adj.get(node)){
-            if(vis[it]==0){
-                dfs(it,adj,vis,st);
-            }
-        }
-        st.push(node);
-    }
+    //Function to return list containing vertices in Topological order. 
     static int[] topoSort(int V, ArrayList<ArrayList<Integer>> adj) {
-        int[] vis = new int[V];
-        Stack<Integer> st = new Stack<>();
+        int indegree[] = new int[V];
         for(int i=0;i<V;i++){
-            if(vis[i]==0){
-                dfs(i,adj,vis,st);
+            for(int it:adj.get(i))
+            indegree[it]++;
+        }
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=0;i<V;i++){
+            if(indegree[i]==0)
+            q.add(i);
+        }
+        int[] topo = new int[V];
+        int i =0;
+        while(!q.isEmpty()){
+            int node = q.remove();
+            topo[i++] = node;
+            for(int it:adj.get(node)){
+                indegree[it]--;
+                if(indegree[it]==0)
+                q.add(it);
             }
         }
-        int ans[] = new int[V];
-        int i = 0;
-        while(!st.isEmpty()){
-            ans[i++] = st.pop();
-        }
-        return ans;
+        return topo;
     }
 }
