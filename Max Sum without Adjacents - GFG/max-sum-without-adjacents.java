32a131194acda26117f1f21c0924ec584a2c3cf4
raiabhi007 <63101700+raiabhi007@users.noreply.github.com>
2023-01-02 10:38:16 +0530

    Added solution - LeetHub

--- Max Sum without Adjacents - GFG/max-sum-without-adjacents.java
+++ Max Sum without Adjacents - GFG/max-sum-without-adjacents.java
@@ -32,24 +32,27 @@ public class Main {
 
 class Solution {
     int findMaxSum(int arr[], int n) {
-         if(n==1)
-        return arr[0];
-        if(n==2)
-        return Math.max(arr[1],arr[0]);
-        int[] dp = new int[n];
-        Arrays.fill(dp,-1);
-        helper(arr,n,dp);
-        return dp[n-1];
+       if(n==1)
+         return arr[0];
+         if(n==2)
+         return Math.max(arr[1],arr[0]); 
+      int[] dp = new int[n];
+      dp[0] = arr[0];
+      dp[1] = Math.max(arr[0],arr[1]);
+      for(int i=2;i<n;i++){
+          dp[i] = Math.max(dp[i-1],arr[i]+dp[i-2]);
+      }
+      return dp[n-1];
     }
-    int helper(int[] arr,int n,int[] dp){
-        if(n==1)
-        return arr[0];
-        if(n==2)
-        return Math.max(arr[1],arr[0]);
-        if(dp[n-1]!=-1)
-        return dp[n-1];
-        int first = helper(arr,n-1,dp);
-        int second = helper(arr,n-2,dp);
-        return dp[n-1] = Math.max(second+arr[n-1],first);
-    }
+    // int helper(int[] arr,int n,int[] dp){
+    //     if(n==1)
+    //     return arr[0];
+    //     if(n==2)
+    //     return Math.max(arr[1],arr[0]);
+    //     if(dp[n-1]!=-1)
+    //     return dp[n-1];
+    //     int first = helper(arr,n-1,dp);
+    //     int second = helper(arr,n-2,dp);
+    //     return dp[n-1] = Math.max(second+arr[n-1],first);
+    // }
 }
