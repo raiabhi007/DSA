b17b25a67c1802ba6325707fd867929723840fb4
raiabhi007 <63101700+raiabhi007@users.noreply.github.com>
2023-01-10 23:19:35 +0530

    Time: 6 ms (84.12%), Space: 42.5 MB (70.38%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,21 +1,22 @@
 class Solution {
     public int minDistance(String word1, String word2) {
-        int dp[][] = new int[word1.length()][word2.length()];
-        for(int i=0;i<word1.length();i++){
-            for(int j=0;j<word2.length();j++)
-                dp[i][j] = -1;
+        int n = word1.length();
+        int m = word2.length();
+        int[][] dp = new int[n+1][m+1];
+        for(int i=0;i<=n;i++)
+            dp[i][0] = i;
+        for(int j=0;j<=m;j++)
+            dp[0][j] = j;
+        
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(word1.charAt(i-1)==word2.charAt(j-1))
+                    dp[i][j] = dp[i-1][j-1];
+                else
+                    dp[i][j] = 1+ Math.min(dp[i-1][j],Math.min(dp[i][j-1],dp[i-1][j-1]));
+            }
         }
-        return helper(word1.length()-1,word2.length()-1,word1,word2,dp);
+        return dp[n][m];
     }
-    public int helper(int i,int j,String s1,String s2,int[][] dp){
-        if(i<0)
-            return j+1;
-        if(j<0)
-            return i+1;
-        if(dp[i][j]!=-1)
-            return dp[i][j];
-        if(s1.charAt(i)==s2.charAt(j))
-            return helper(i-1,j-1,s1,s2,dp);
-        return dp[i][j] = 1+Math.min(helper(i-1,j-1,s1,s2,dp),Math.min(helper(i-1,j,s1,s2,dp),helper(i,j-1,s1,s2,dp)));
-        }
 }
