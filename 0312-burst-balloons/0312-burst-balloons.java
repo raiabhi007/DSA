34c901ff60893a03522fdabe83d2d0b744294194
raiabhi007 <63101700+raiabhi007@users.noreply.github.com>
2023-01-15 00:04:04 +0530

    Time: 40 ms (95.31%), Space: 42 MB (71.39%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.java
+++ 0312-burst-balloons/0312-burst-balloons.java
@@ -7,22 +7,28 @@ class Solution {
         for(int i=1;i<=n;i++)
             arr[i] = nums[i-1];
         int dp[][] = new int[n+2][n+2];
-        for(int i=0;i<n+2;i++){
-            for(int j=0;j<n+2;j++){
-                dp[i][j] = -1;
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=n;j++){
+                if(i>j) continue;
+                int max = Integer.MIN_VALUE;
+                for(int ind=i;ind<=j;ind++){
+                    int cost = arr[i-1]*arr[ind]*arr[j+1]+dp[i][ind-1]+dp[ind+1][j];
+                    max = Math.max(cost,max);
+                }
+                dp[i][j] = max;
             }
         }
-        return helper(1,n,arr,dp);
+        return dp[1][n];
     }
-    public int helper(int i,int j, int[] arr,int[][] dp){
-        if(i>j) return 0;
-        int max = Integer.MIN_VALUE;
-        if(dp[i][j]!=-1)
-            return dp[i][j];
-        for(int ind=i;ind<=j;ind++){
-            int cost = arr[i-1]*arr[ind]*arr[j+1]+helper(i,ind-1,arr,dp)+helper(ind+1,j,arr,dp);
-            max = Math.max(cost,max);
-        }
-        return dp[i][j]=max;
-    }
+    // public int helper(int i,int j, int[] arr,int[][] dp){
+    //     if(i>j) return 0;
+    //     int max = Integer.MIN_VALUE;
+    //     if(dp[i][j]!=-1)
+    //         return dp[i][j];
+    //     for(int ind=i;ind<=j;ind++){
+    //         int cost = arr[i-1]*arr[ind]*arr[j+1]+helper(i,ind-1,arr,dp)+helper(ind+1,j,arr,dp);
+    //         max = Math.max(cost,max);
+    //     }
+    //     return dp[i][j]=max;
+    // }
 }
