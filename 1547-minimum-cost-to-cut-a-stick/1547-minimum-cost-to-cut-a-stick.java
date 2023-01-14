8470b046e1bde8326272ca4a91fdc7ce424fc95e
raiabhi007 <63101700+raiabhi007@users.noreply.github.com>
2023-01-14 23:09:51 +0530

    Time: 10 ms (98.31%), Space: 42.1 MB (67.03%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.java
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.java
@@ -1,28 +1,39 @@
 class Solution {
     public int minCost(int n, int[] cut) {
-        int cuts[] = new int[cut.length+2];
+        int c = cut.length;
+        int cuts[] = new int[c+2];
         int i=0;
-        for(i=0;i<cut.length;i++){
+        for(i=0;i<c;i++){
             cuts[i] = cut[i];
         }
         cuts[i++] = 0;
         cuts[i] = n;
         Arrays.sort(cuts);
-        int dp[][] = new int[cuts.length][cuts.length];
-        for(i=0;i<cuts.length;i++){
-          Arrays.fill(dp[i],-1);
+        int dp[][] = new int[c+2][c+2];
+        for(i=c;i>=1;i--){
+            for(int j=1;j<=c;j++){
+                if(i>j) continue;
+                int mini = Integer.MAX_VALUE;
+                for(int ind=i;ind<=j;ind++){
+                    int cost = cuts[j+1]-cuts[i-1] + dp[i][ind-1]+dp[ind+1][j];
+                    mini = Math.min(mini,cost);
+                }
+                dp[i][j] = mini;
+            }
         }
-        return helper(1,cut.length,cuts,dp);
+        
+        return dp[1][c];
     }
-    public int helper(int i,int j,int[] cuts,int[][] dp){
-        if(i>j) return 0;
-        if(dp[i][j]!=-1)
-            return dp[i][j];
-        int mini = Integer.MAX_VALUE;
-        for(int ind=i;ind<=j;ind++){
-            int cost = cuts[j+1]-cuts[i-1] + helper(i,ind-1,cuts,dp)+helper(ind+1,j,cuts,dp);
-            mini = Math.min(cost,mini);
-        }
-        return dp[i][j]=mini;
-    }
-}
+    
+//     public int helper(int i,int j,int[] cuts,int[][] dp){
+//         if(i>j) return 0;
+//         if(dp[i][j]!=-1)
+//             return dp[i][j];
+//         int mini = Integer.MAX_VALUE;
+//         for(int ind=i;ind<=j;ind++){
+//             int cost = cuts[j+1]-cuts[i-1] + helper(i,ind-1,cuts,dp)+helper(ind+1,j,cuts,dp);
+//             mini = Math.min(cost,mini);
+//         }
+//         return dp[i][j]=mini;
+//     }    
+ }
