aa45be1954b63e8b4957a0cbf8506d3ad9e15da7
raiabhi007 <63101700+raiabhi007@users.noreply.github.com>
2023-01-16 01:23:37 +0530

    Time: 199 ms (46.00%), Space: 40.4 MB (87.72%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
@@ -1,7 +1,7 @@
 class Solution {
-     boolean isPalindrome(String str,int i,int j){
+    boolean isPalindrome(char[] str,int i,int j){
         while(i<=j){
-            if(str.charAt(i)!=str.charAt(j))
+            if(str[i]!=str[j])
             return false;
             i++;
             j--;
@@ -9,13 +9,14 @@ class Solution {
         return true;
     }
     public int minCut(String s) {
+        char[] str = s.toCharArray();
         int n = s.length();
         int[] dp = new int[n+1];
         dp[n] = 0;
         for(int i=n-1;i>=0;i--){
             int minCost = Integer.MAX_VALUE;
             for(int j=i;j<n;j++){
-                if(isPalindrome(s,i,j)){
+                if(isPalindrome(str,i,j)){
                     int cost = 1+dp[j+1];
                     minCost = Math.min(cost,minCost);
                 }
