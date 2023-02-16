21c90e7e88075001f50b60a7975793e505bd0b52
raiabhi007 <63101700+raiabhi007@users.noreply.github.com>
2023-02-16 10:55:03 +0530

    Added solution - LeetHub

---
+++ Search Pattern (KMP-Algorithm) - GFG/search-pattern-kmpalgorithm.java
@@ -0,0 +1,106 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0)
+        {
+            String s, patt;
+            s = sc.next();
+            patt = sc.next();
+            
+            Solution ob = new Solution();
+            
+            ArrayList<Integer> res = ob.search(patt, s);
+            if(res.size()==0)
+                System.out.print(-1);
+            else {
+                for(int i = 0;i<res.size();i++)
+                    System.out.print(res.get(i) + " ");
+            }
+            System.out.println();    
+        }
+    }
+}
+// } Driver Code Ends
+
+class Solution{
+    ArrayList<Integer> search(String pat, String txt){
+        ArrayList<Integer> ans = new ArrayList<>();
+        int M = pat.length();
+        int N = txt.length();
+
+        // create lps[] that will hold the longest
+        // prefix suffix values for pattern
+        int lps[] = new int[M];
+        int j = 0; // index for pat[]
+
+        // Preprocess the pattern (calculate lps[]
+        // array)
+        computeLPSArray(pat, M, lps);
+
+        int i = 0; // index for txt[]
+        while ((N - i) >= (M - j)) {
+            if (pat.charAt(j) == txt.charAt(i)) {
+                j++;
+                i++;
+            }
+            if (j == M) {
+                ans.add(i-j+1);
+                j = lps[j - 1];
+            }
+            // mismatch after j matches
+            else if (i < N
+                     && pat.charAt(j) != txt.charAt(i)) {
+                // Do not match lps[0..lps[j-1]] characters,
+                // they will match anyway
+                if (j != 0)
+                    j = lps[j - 1];
+                else
+                    i = i + 1;
+            }
+        }
+        if(ans.size()==0)
+        ans.add(-1);
+        return ans;
+    }
+      void computeLPSArray(String pat, int M, int lps[]){
+        // length of the previous longest prefix suffix
+        int len = 0;
+        int i = 1;
+        lps[0] = 0; // lps[0] is always 0
+
+        // the loop calculates lps[i] for i = 1 to M-1
+        while (i < M) {
+            if (pat.charAt(i) == pat.charAt(len)) {
+                len++;
+                lps[i] = len;
+                i++;
+            }
+            else // (pat[i] != pat[len])
+            {
+                // This is tricky. Consider the example.
+                // AAACAAAA and i = 7. The idea is similar
+                // to search step.
+                if (len != 0) {
+                    len = lps[len - 1];
+
+                    // Also, note that we do not increment
+                    // i here
+                }
+                else // if (len == 0)
+                {
+                    lps[i] = len;
+                    i++;
+                }
+            }
+        }
+    }
+}
