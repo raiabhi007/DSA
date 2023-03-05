e41880a97cd74a1777329b528def365356e7239c
raiabhi007 <63101700+raiabhi007@users.noreply.github.com>
2023-03-05 10:00:36 +0530

    Time: 4 ms (57.49%), Space: 42.3 MB (82.25%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,13 +1,13 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        int[] hash = new int[256];
+        int[] hash = new int[26];
         for(int i=0;i<s.length();i++){
             char ch = s.charAt(i);
-            hash[ch]++;
+            hash[ch-'a']++;
         }
          for(int i=0;i<t.length();i++){
             char ch = t.charAt(i);
-            hash[ch]--;
+            hash[ch-'a']--;
         }
         for(int i=0;i<hash.length;i++){
             if(hash[i]!=0)
