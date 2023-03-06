f3a061f5dbddf3c2101e0d25b239ee0da82f65af
raiabhi007 <63101700+raiabhi007@users.noreply.github.com>
2023-03-06 08:45:15 +0530

    Added solution - LeetHub

---
+++ Maximum Nesting Depth of the Parentheses - GFG/maximum-nesting-depth-of-the-parentheses.java
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc=new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0){
+            sc.nextLine();
+            String s;
+            s = sc.next();
+            
+            Solution obj = new Solution();
+            int res = obj.maxDepth(s);
+            
+            System.out.println(res);
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public static int maxDepth(String s) {
+         int count = 0;   //count current dept of "()"
+        int max = 0;     //count max dept of "()"
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                count++;
+            } else if (s.charAt(i) == ')') {
+                count--;
+            }
+            max = Math.max(count, max);
+        }
+        return max;
+    }
+}
+        
