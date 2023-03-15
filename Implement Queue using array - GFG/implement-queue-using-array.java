29e4d302971216e96a9755962eeae842bdfc6750
raiabhi007 <63101700+raiabhi007@users.noreply.github.com>
2023-03-15 09:08:07 +0530

    Added solution - LeetHub

---
+++ Implement Queue using array - GFG/implement-queue-using-array.java
@@ -0,0 +1,77 @@
+//{ Driver Code Starts
+import java.util.Scanner;
+
+class GfG
+{
+	public static void main(String args[])
+	{
+		 Scanner sc = new Scanner(System.in);
+		 int t=sc.nextInt();
+		 while(t>0)
+		 {
+			MyQueue obj = new MyQueue();
+			int Q = sc.nextInt();
+			while(Q>0)
+			{
+				int QueryType = 0;
+				QueryType = sc.nextInt();
+				if(QueryType == 1)
+				{
+					int a = sc.nextInt();
+					
+					obj.push(a);
+					
+				}else if(QueryType == 2)
+				{
+				System.out.print(obj.pop()+" ");
+				}
+				Q--;
+			}
+			System.out.println("");
+			t--;
+		 }
+	}
+}
+
+
+
+
+// } Driver Code Ends
+class MyQueue {
+    int front, rear,size;
+	int arr[] = new int[100005];
+    MyQueue(){
+		front=-1;
+		rear=-1;
+		size = 0;
+	}
+	//Function to push an element x in a queue.
+	void push(int x){
+	    if(size==arr.length)
+	    return;
+	    if(size==0){
+	        front = 0;
+	    }
+	    size++;
+	    rear = (rear+1)%arr.length;
+	    arr[rear] = x;
+	    
+	} 
+    //Function to pop an element from queue and return that element.
+	int pop(){
+		if(size==0)
+		return -1;
+		int temp = arr[front];
+		size--;
+		front = (front+1)%arr.length;
+		if(size==0){
+		front = -1;
+		rear = -1;
+		}
+		return temp;
+	} 
+}
+
+
+
+
