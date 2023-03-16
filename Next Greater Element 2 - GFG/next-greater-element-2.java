75f437771bce30c5fb7fa961feee8a79616b2446
raiabhi007 <63101700+raiabhi007@users.noreply.github.com>
2023-03-16 06:47:40 +0530

    Added solution - LeetHub

---
+++ Next Greater Element 2 - GFG/next-greater-element-2.java
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.math.*;
+import java.io.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int n = sc.nextInt();
+            int a[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = sc.nextInt();
+            }
+            Solution obj = new Solution();
+            int ans[] = obj.nextGreaterElement(n, a);
+            for (int e : ans) System.out.print(e + " ");
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    static int[] nextGreaterElement(int N, int nums[]) {
+        int ans[] = new int[nums.length];
+         Stack<Integer> st = new Stack<>();
+        for(int i=nums.length-1;i>=0;i--)
+            st.push(nums[i]);
+        
+        for(int i=nums.length-1;i>=0;i--){
+            while(!st.isEmpty()&&nums[i]>=st.peek())
+                st.pop();
+            if(st.isEmpty())
+                ans[i] = -1;
+            else
+                ans[i] = st.peek();
+            st.push(nums[i]);
+        }
+        return ans;
+    }
+}
