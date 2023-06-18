9393306bfc4e59175f4a15136e99779ef16bec99
Abhishek Rai <63101700+raiabhi007@users.noreply.github.com>
2023-06-18 07:44:35 +0530

    Time: 0 ms (100.00%), Space: 41 MB (19.57%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.java
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.java
@@ -17,33 +17,22 @@ class Solution {
     public TreeNode bstFromPreorder(int[] preorder) {
         return helper(preorder,0,preorder.length-1);
     }
-    public TreeNode helper(int[] pre,int start,int end){    
-        if(start>end)
+    public TreeNode helper(int[] pre, int start,int end){
+        if(end<start)
             return null;
         TreeNode root = new TreeNode(pre[start]);
-        if(start==end)
-            return root;
-        int midIndex = -1;
-        for(int i=start+1;i<=end;i++){
-            if(pre[i]>pre[start]){
-                midIndex = i;
+        int idx = start+1;
+        int i = start+1;
+        for(i=start+1;i<=end;i++){
+            if(pre[i]>root.val){
+                idx = i;
                 break;
-            }    
-        }        
-        int leftS=start+1;
-        int leftE= midIndex-1;
-        int rightS=midIndex;
-        int rightE=end;
-        if(midIndex==-1){
-            root.left = helper(pre,start+1,end);
-            root.right = null;
-        }else if(midIndex==start+1){
-            root.left = null;
-            root.right = helper(pre,start+1,end);
-        }else{
-        root.left = helper(pre,leftS,leftE);
-        root.right = helper(pre,rightS,rightE);
+            }
         }
+        if(i>end)
+            idx = end+1;
+        root.left = helper(pre,start+1,idx-1);
+        root.right = helper(pre,idx,end);
         return root;
     }
 }
