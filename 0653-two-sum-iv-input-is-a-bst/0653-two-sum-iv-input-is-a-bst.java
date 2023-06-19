e34bcc5314aa3248b397f512e6e7a3c92ecc33f7
Abhishek Rai <63101700+raiabhi007@users.noreply.github.com>
2023-06-19 07:59:05 +0530

    Time: 3 ms (93.68%), Space: 44.1 MB (20.16%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
@@ -1,31 +1,24 @@
-
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-     public boolean findTarget(TreeNode root, int k) {
-        if (root == null) return false;
-        
-        List<Integer> list = new ArrayList<>();
-        inOrder(root, list);
-        
-        int i = 0, j = list.size() - 1;
-        while (i < j) {
-            int sum = list.get(i) + list.get(j);
-            if (sum == k) return true;
-            if (sum < k) {
-                i++;
-            }
-            else {
-                j--;
-            }
-        }
-        
-        return false;
-    }
-    
-    private void inOrder(TreeNode root, List<Integer> list) {
-        if (root == null) return;
-        
-        inOrder(root.left, list);
-        list.add(root.val);
-        inOrder(root.right, list);
+    HashSet set = new HashSet<>();
+    public boolean findTarget(TreeNode root, int k) {
+          if(root == null) return false;
+          if(set.contains(root.val)) return true;
+          set.add(k-root.val);
+          return findTarget(root.left,k) || findTarget(root.right,k);
     }
 }
