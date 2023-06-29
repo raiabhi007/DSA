2dda8fac747c1bc0c44ac934af77a7b06953da19
Abhishek Rai <63101700+raiabhi007@users.noreply.github.com>
2023-06-29 09:28:44 +0530

    Added solution - LeetHub

--- Flood fill Algorithm - GFG/flood-fill-algorithm.java
+++ Flood fill Algorithm - GFG/flood-fill-algorithm.java
@@ -42,18 +42,19 @@ class Solution
     public int[][] floodFill(int[][] image, int sr, int sc, int newColor){
          boolean visited[][] = new boolean[image.length][image[0].length];
         int oldColor = image[sr][sc];
-        dfs(image,sr,sc,newColor,oldColor,visited);
+        if(oldColor==newColor)
+        return image;
+        dfs(image,sr,sc,newColor,oldColor);
         return image;
     }
-    public void dfs(int[][] image,int sr,int sc,int newColor,int oldColor,boolean[][] visited){
-        if(sr<0||sc<0||sr>=image.length||sc>=image[0].length||visited[sr][sc]==true||image[sr][sc]!=oldColor)
+    public void dfs(int[][] image,int sr,int sc,int newColor,int oldColor){
+        if(sr<0||sc<0||sr>=image.length||sc>=image[0].length||image[sr][sc]!=oldColor)
             return;
             image[sr][sc] = newColor;
-            visited[sr][sc] = true;
-            dfs(image,sr-1,sc,newColor,oldColor,visited);
-            dfs(image,sr,sc-1,newColor,oldColor,visited);
-            dfs(image,sr+1,sc,newColor,oldColor,visited);
-            dfs(image,sr,sc+1,newColor,oldColor,visited);
+            dfs(image,sr-1,sc,newColor,oldColor);
+            dfs(image,sr,sc-1,newColor,oldColor);
+            dfs(image,sr+1,sc,newColor,oldColor);
+            dfs(image,sr,sc+1,newColor,oldColor);
         
     }
 }
