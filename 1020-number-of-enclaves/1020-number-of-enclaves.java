af5f3c6e60eb31a6fd95567fdad89f34c8b33a29
Abhishek Rai <63101700+raiabhi007@users.noreply.github.com>
2023-06-30 10:47:59 +0530

    Time: 10 ms (69.09%), Space: 54.1 MB (82.60%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.java
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -1,40 +1,40 @@
 class Solution {
     public int numEnclaves(int[][] grid) {
-        int[][] visited = new int[grid.length][grid[0].length];
-        int ans = 0;
+        int n = grid.length;
+        int m = grid[0].length;
+        boolean vis[][] = new boolean[n][m];
+        for(int i=0;i<n;i++){
+            if(grid[i][0]==1)
+                dfs(grid,i,0,vis);
+            if(grid[i][m-1]==1)
+                dfs(grid,i,m-1,vis);
+        }
         
-        for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[0].length;j++){
-                if(visited[i][j]==0||visited[i][j]==-1){
-                    int temp = dfs(grid,visited,i,j);
-                        if(temp!=-1)
-                            ans+=temp;
-                }
+         for(int j=0;j<m;j++){
+            if(grid[0][j]==1)
+                dfs(grid,0,j,vis);
+            if(grid[n-1][j]==1)
+                dfs(grid,n-1,j,vis);
+        }
+        int ans = 0;
+        for(int i=1;i<n-1;i++){
+            for(int j=1;j<m-1;j++){
+                if(grid[i][j]==1&&vis[i][j]==false)
+                    ans++;
             }
         }
         return ans;
-     }
-    public int dfs(int[][] grid,int[][] visited,int i,int j){
-        if(i<0||j<0||i==grid.length||j==grid[0].length||visited[i][j]==1||grid[i][j]==0)
-            return 0;
-        if(visited[i][j]==-1)
-            return -1;
-        
-        if((i==0||j==0||i==grid.length-1||j==grid[0].length-1)&&grid[i][j]==1){
-            visited[i][j] = -1;
-            return -1;
+    }
+    void dfs(int[][] grid,int i,int j,boolean[][] vis){
+        if(i<0||i>=grid.length||j<0||j>=grid[0].length||grid[i][j]==0||vis[i][j]==true){
+            return;
         }
-         visited[i][j] = 1;
-         int left = dfs(grid,visited,i,j-1);
-         int right = dfs(grid,visited,i,j+1);
-         int top = dfs(grid,visited,i-1,j);
-         int bottom = dfs(grid,visited,i+1,j);
-         if(left==-1||right==-1||top==-1||bottom==-1){
-             visited[i][j] = -1;
-             return -1;
-         }
-       
-         return 1+top+bottom+left+right;
-            
+        
+        vis[i][j]=true;
+        dfs(grid,i-1,j,vis);
+        dfs(grid,i,j-1,vis);
+        dfs(grid,i+1,j,vis);
+        dfs(grid,i,j+1,vis);
+        
     }
 }
