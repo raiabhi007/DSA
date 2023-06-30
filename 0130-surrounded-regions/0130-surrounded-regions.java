a80a912f4558a9cd6aaa83141070ada200122e57
Abhishek Rai <63101700+raiabhi007@users.noreply.github.com>
2023-06-30 10:21:05 +0530

    Time: 1 ms (98.44%), Space: 44.5 MB (40.65%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -1,44 +1,36 @@
 class Solution {
-    static void dfs(int row,int col,int[][] vis,char[][] mat,int delrow[],int[] delcol){
-        vis[row][col] = 1;
-        int n = mat.length;
-        int m = mat[0].length;
-        for(int i=0;i<4;i++){
-            int nrow = row+delrow[i];
-            int ncol = col+delcol[i];
-            if(nrow>=0&& nrow<n&&ncol>=0&&ncol<m&&vis[nrow][ncol]==0&&mat[nrow][ncol]=='O')
-                dfs(nrow,ncol,vis,mat,delrow,delcol);
-        }
-    }
-    public void solve(char[][] mat) {
-        int n = mat.length;
-        int m = mat[0].length;
-        int delrow[] = {-1,0,1,0};
-        int delcol[] = {0,1,0,-1};
-        int vis[][] = new int[n][m];
-        //traverse first row and last row
-        for(int j=0;j<m;j++){
-            //first row
-            if(vis[0][j]==0&&mat[0][j]=='O')
-                dfs(0,j,vis,mat,delrow,delcol);
-           //last row
-            if(vis[n-1][j]==0&&mat[n-1][j]=='O')
-                dfs(n-1,j,vis,mat,delrow,delcol);
-        }
+    public void solve(char[][] board) {
+        int n = board.length;
+        int m = board[0].length;
+        boolean[][] vis = new boolean[board.length][board[0].length];
         for(int i=0;i<n;i++){
-            //first column
-            if(vis[i][0]==0&&mat[i][0]=='O')
-                dfs(i,0,vis,mat,delrow,delcol);
-            //last column
-            if(vis[i][m-1]==0&&mat[i][m-1]=='O')
-            dfs(i,m-1,vis,mat,delrow,delcol);
+            if(board[i][0]=='O')
+                dfs(board,i,0,vis);
+            if(board[i][m-1]=='O')
+                dfs(board,i,m-1,vis);
         }
-        
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(vis[i][j]==0&&mat[i][j]=='O')
-                    mat[i][j] = 'X';
+         for(int j=0;j<m;j++){
+            if(board[0][j]=='O')
+                dfs(board,0,j,vis);
+            if(board[n-1][j]=='O')
+                dfs(board,n-1,j,vis);
+        }
+        for(int i=1;i<n-1;i++){
+            for(int j=1;j<m-1;j++){
+                if(vis[i][j]==false&&board[i][j]=='O')
+                    board[i][j] = 'X';
             }
         }
+        
     }
+    void dfs(char[][] board, int i, int j,boolean[][] vis){
+        if(i<0||i>=vis.length||j<0||j>=vis[0].length||board[i][j]=='X'||vis[i][j]==true)
+            return;
+        vis[i][j] = true;
+        dfs(board,i-1,j,vis);
+        dfs(board,i,j-1,vis);
+        dfs(board,i+1,j,vis);
+        dfs(board,i,j+1,vis);
+    }
+    
 }
