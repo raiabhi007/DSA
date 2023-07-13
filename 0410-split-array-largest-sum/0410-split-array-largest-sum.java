61e64b924e6f4029ac816ca4e3b8d9b29f1c17c6
Abhishek Rai <63101700+raiabhi007@users.noreply.github.com>
2023-07-13 07:57:02 +0530

    Time: 0 ms (100.00%), Space: 40.3 MB (52.83%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.java
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.java
@@ -1,15 +1,15 @@
 class Solution {
     public int splitArray(int[] nums, int k) {
-        int low = Integer.MAX_VALUE;
+        int low = -1;
         int high = 0;
         for(int i=0;i<nums.length;i++){
-            low = Math.min(low,nums[i]);
             high+=nums[i];
+            low = Math.max(low,nums[i]);
         }
         int ans = 0;
         while(low<=high){
-            int mid = low+(high-low)/2;
-            if(isPossible(nums,k,mid)){
+            int mid = (low+high)/2;
+            if(isPossible(nums,mid,k)){
                 ans = mid;
                 high = mid-1;
             }else
@@ -17,20 +17,16 @@ class Solution {
         }
         return ans;
     }
-    public boolean isPossible(int arr[], int k, int sum){
+    boolean isPossible(int[] arr,int mid,int k){
         int tempSum = 0;
-        int tempK = 1;
+        int count = 0;
         for(int i=0;i<arr.length;i++){
-            if(sum<arr[i])
-                return false;
-            if(tempSum+arr[i]>sum){
-                tempK++;
+            if(tempSum+arr[i]>mid){
+                count++;
                 tempSum = arr[i];
             }else
                 tempSum+=arr[i];
         }
-        if(tempK>k)
-            return false;
-        return true;
+        return count<k;
     }
 }
