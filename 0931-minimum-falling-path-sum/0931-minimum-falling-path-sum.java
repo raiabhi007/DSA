eae2015126062d06028ffecb96a6cd5718a73138
Abhishek Rai <63101700+raiabhi007@users.noreply.github.com>
2023-07-16 08:38:29 +0530

    Time: 5 ms (64.63%), Space: 43.9 MB (83.16%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
@@ -1,28 +1,22 @@
 class Solution {
-   public int helper(int i,int j,int n,int [][]matrix,int dp[][])
-    {//memo
-        if(j<0 || j>=n) return (int)Math.pow(10,9);//for  preventing out of bound 
-        if(i==0)return matrix[i][j];// once we reach the 1st row we will return element\
-        if(dp[i][j]!=-1) return dp[i][j];
-        int up=matrix[i][j]+helper(i-1,j,n,matrix,dp);
-        int upleft=matrix[i][j]+helper(i-1,j-1,n,matrix,dp);
-        int upright=matrix[i][j]+helper(i-1,j+1,n,matrix,dp);
-        return dp[i][j]=Math.min(up,Math.min(upleft,upright));
-    }
     public int minFallingPathSum(int[][] matrix) {
-     int  m=matrix.length;
-        int n=matrix[0].length;
-        int dp[][]=new int[m+1][n+1];
-        for(int []row:dp)
-        {
-            Arrays.fill(row,-1);
+        int n = matrix.length;
+        int[][] dp = new int[n][n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i==0)
+                    dp[i][j] = matrix[i][j];
+                else if(j==0){
+                    dp[i][j] = matrix[i][j]+Math.min(dp[i-1][j],dp[i-1][j+1]);
+                }else if(j==n-1)
+                    dp[i][j] = matrix[i][j]+Math.min(dp[i-1][j],dp[i-1][j-1]);
+                else
+                    dp[i][j] = matrix[i][j]+Math.min(dp[i-1][j-1],Math.min(dp[i-1][j],dp[i-1][j+1]));
+            }
         }
-        int maxi=Integer.MAX_VALUE;
-        for(int j=0;j<n;j++)
-        {
-            int ans=helper(m-1,j,n,matrix,dp);
-            maxi=Math.min(maxi,ans);
-        }
-        return maxi;
+        int ans = Integer.MAX_VALUE;
+        for(int i=n-1;i>=0;i--)
+            ans = Math.min(ans,dp[n-1][i]);
+        return ans;
     }
 }
