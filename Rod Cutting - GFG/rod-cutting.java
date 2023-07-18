c2dcc49a0e41815f933deca4f58166a0c8ba6be2
Abhishek Rai <63101700+raiabhi007@users.noreply.github.com>
2023-07-18 09:46:59 +0530

    Added solution - LeetHub

--- Rod Cutting - GFG/rod-cutting.java
+++ Rod Cutting - GFG/rod-cutting.java
@@ -1,41 +1,41 @@
-// { Driver Code Starts
-
-
+//{ Driver Code Starts
 import java.io.*;
 import java.util.*;
 
 class RodCutting {
 
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
+    public static void main(String args[])throws IOException {
+        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        
+        int t = Integer.parseInt(in.readLine().trim());
         while (t-- > 0) {
-            int n = sc.nextInt();
+            int n = Integer.parseInt(in.readLine().trim());
+            String s[]=in.readLine().trim().split(" ");
             int[] arr = new int[n];
-            for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
+            for (int i = 0; i < n; i++) arr[i] = Integer.parseInt(s[i]);
 
             Solution ob = new Solution();
-            System.out.println(ob.cutRod(arr, n));
+            out.println(ob.cutRod(arr, n));
         }
+        out.close();
     }
 }
+
 // } Driver Code Ends
+
+
 class Solution{
     public int cutRod(int price[], int n) {
-        int dp[] = new int[n+1];
-        Arrays.fill(dp,-1);
-        dp[0] = 0;
-        return helper(price,n,dp);
-    }
-    public int helper(int price[],int n,int dp[]){
-        if(dp[n]!=-1)
-        return dp[n];
-        
-        int ans = 0;
-        for(int i=0;i<n;i++){
-            int temp = price[i] +  helper(price,n-(i+1),dp);
-            ans = Math.max(temp,ans);
-        }
-        return dp[n] = ans;
+       int[] dp = new int[n+1];
+       for(int i=1;i<=n;i++){
+           int ans = 0;
+           for(int j=0;j<i;j++){
+               int temp = price[j]+dp[i-(j+1)];
+               ans = Math.max(temp,ans);
+           }
+           dp[i]=ans;
+       }
+       return dp[n];
     }
 }
