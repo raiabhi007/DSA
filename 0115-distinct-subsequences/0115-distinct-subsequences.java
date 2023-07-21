730031f55389cab9ab1e3979f9cc5a0c9207179a
Abhishek Rai <63101700+raiabhi007@users.noreply.github.com>
2023-07-21 10:11:41 +0530

    Time: 22 ms (54.84%), Space: 47 MB (56.45%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,26 +1,21 @@
 class Solution {
     public int numDistinct(String s, String t) {
-       int n = s.length();
-       int m = t.length();
-       int dp[][]=new int[n+1][m+1];
-    
-        for(int i=0;i<n+1;i++){
-        dp[i][0]=1;
-       }
-        
-        for(int i=1;i<m+1;i++){
-        dp[0][i]=0;
-        }
-        
-        for(int i=1;i<n+1;i++){
-              for(int j=1;j<m+1;j++){
-            
-            if(s.charAt(i-1)==t.charAt(j-1))
-                dp[i][j] = (dp[i-1][j-1] + dp[i-1][j]);
-            else
-                dp[i][j] = dp[i-1][j];
-           }
-       } 
-        return dp[n][m];
+        int dp[][]  = new int[s.length()+1][t.length()+1];
+        for(int i=0;i<s.length();i++)
+        Arrays.fill(dp[i],-1);
+        return helper(s,t,0,0,dp);
+    }
+    public int helper(String s, String t, int i,int j,int[][] dp){
+        if(j==t.length())
+            return 1;
+        if(i==s.length())
+            return 0;
+        if(dp[i][j]!=-1)
+            return dp[i][j];
+        int inc = 0;
+        if(s.charAt(i)==t.charAt(j))
+            inc+=helper(s,t,i+1,j+1,dp);
+        int exc = helper(s,t,i+1,j,dp);
+        return dp[i][j]=inc+exc;
     }
 }
