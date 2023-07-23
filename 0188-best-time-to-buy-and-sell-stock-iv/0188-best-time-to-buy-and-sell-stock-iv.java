7b43de6b50c7cf8b80758ef7da646336f0613b8f
Abhishek Rai <63101700+raiabhi007@users.noreply.github.com>
2023-07-23 10:01:21 +0530

    Time: 10 ms (35.93%), Space: 43.4 MB (46.09%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -1,20 +1,28 @@
 class Solution {
-    public int maxProfit(int k, int[] arr) {
-         if(arr.length==0)
-         return 0;   
-        int n = arr.length;
-        int[][] dp = new int[k+1][n];
-        for(int t=1;t<=k;t++){
-            int max = Integer.MIN_VALUE;
-            for(int d=1;d<arr.length;d++){
-                if(dp[t-1][d-1]-arr[d-1]>max)
-                    max = dp[t-1][d-1] - arr[d-1];
-                if(max+arr[d]>dp[t][d-1])
-                    dp[t][d] = max + arr[d];
-                else
-                    dp[t][d] = dp[t][d-1];
+    public int maxProfit(int k, int[] prices) {
+         int[][][] dp = new int[prices.length][2][k+1];
+        for(int i=0;i<prices.length;i++){
+            for(int j=0;j<=1;j++){
+                for(int l=0;l<=k;l++){
+                    dp[i][j][l]=-1;
+                }
             }
         }
-        return dp[k][n-1];
+        return func(prices,0,0,k,dp);
+    }
+    public int func(int[] prices,int idx,int buy,int k,int[][][] dp){
+        if(k==0) return 0;
+        if(idx==prices.length) return 0;
+        if(dp[idx][buy][k]!=-1)
+            return dp[idx][buy][k];
+        if(buy==0){
+            int op1 = func(prices,idx+1,0,k,dp);
+            int op2 = -prices[idx]+func(prices,idx+1,1,k,dp);
+            return dp[idx][buy][k]=Math.max(op1,op2);
+        }else{
+            int op1 = func(prices,idx+1,1,k,dp);
+            int op2 = prices[idx]+func(prices,idx+1,0,k-1,dp);
+            return dp[idx][buy][k]=Math.max(op1,op2);
+        }
     }
 }
