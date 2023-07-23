3fdab6f9ea6aa2988bef67c7e4bbb3b1cfed4ce8
Abhishek Rai <63101700+raiabhi007@users.noreply.github.com>
2023-07-23 12:27:47 +0530

    Time: 2 ms (19.05%), Space: 41.3 MB (14.74%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,29 +1,28 @@
 class Solution {
-    public int maxProfit(int[] arr) {
-        int obsp = -arr[0];
-        int ossp = 0;
-        int ocsp = 0;
-        for(int i=1;i<arr.length;i++){
-            int nssp = 0;
-            int nbsp = 0;
-            int ncsp = 0;
-            if(ocsp-arr[i]>obsp){
-                nbsp = ocsp-arr[i];
-            }else
-                nbsp = obsp;
-            if(arr[i]+obsp>ossp)
-                nssp = arr[i]+obsp;
-            else
-                nssp = ossp;
-            if(ossp>ocsp){
-                ncsp = ossp;
+    public int maxProfit(int[] prices) {
+        int dp[][][] = new int[prices.length][2][2];
+        for(int i=0;i<prices.length;i++){
+            for(int j=0;j<=1;j++){
+                for(int k=0;k<=1;k++)
+                    dp[i][j][k] = -1;
             }
-            else
-                ncsp = ocsp;
-            obsp = nbsp;
-            ossp = nssp;
-            ocsp = ncsp;
         }
-        return ossp;
+        return func(prices,0,0,0,dp);
+    }
+    public int func(int[] prices,int idx,int buy,int cooldown,int[][][]dp){
+        if(idx==prices.length) return 0;
+        if(dp[idx][buy][cooldown]!=-1) return dp[idx][buy][cooldown];
+        
+        if(cooldown==1){
+            return dp[idx][buy][cooldown]=func(prices,idx+1,buy,0,dp);
+        }else if(buy==0){
+            int op1 = func(prices,idx+1,0,0,dp);
+            int op2 = -prices[idx]+func(prices,idx+1,1,0,dp);
+            return dp[idx][buy][cooldown]=Math.max(op1,op2);
+        }else{
+            int op1 = func(prices,idx+1,1,0,dp);
+            int op2 = prices[idx]+func(prices,idx+1,0,1,dp);
+            return dp[idx][buy][cooldown]=Math.max(op1,op2);
+        }
     }
 }
