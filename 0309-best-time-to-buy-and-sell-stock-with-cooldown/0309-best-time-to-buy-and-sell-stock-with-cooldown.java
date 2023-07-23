f93566b6afd3f42123ccd77a22aba0598c7a1f1f
Abhishek Rai <63101700+raiabhi007@users.noreply.github.com>
2023-07-23 12:47:05 +0530

    Time: 1 ms (84.10%), Space: 40.6 MB (80.11%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,28 +1,24 @@
 class Solution {
-    public int maxProfit(int[] prices) {
-        int dp[][][] = new int[prices.length][2][2];
-        for(int i=0;i<prices.length;i++){
-            for(int j=0;j<=1;j++){
-                for(int k=0;k<=1;k++)
-                    dp[i][j][k] = -1;
+    public int maxProfit(int[] Arr) {
+          int n = Arr.length;
+    int dp[][]=new int[n+2][2];
+    
+    for(int ind = n-1; ind>=0; ind--){
+        for(int buy=0; buy<=1; buy++){
+            int profit=0;
+            
+            if(buy==0){// We can buy the stock
+                profit = Math.max(0+dp[ind+1][0], -Arr[ind] + dp[ind+1][1]);
             }
+    
+            if(buy==1){// We can sell the stock
+                profit = Math.max(0+dp[ind+1][1], Arr[ind] + dp[ind+2][0]);
+            }
+            
+            dp[ind][buy] = profit;
         }
-        return func(prices,0,0,0,dp);
     }
-    public int func(int[] prices,int idx,int buy,int cooldown,int[][][]dp){
-        if(idx==prices.length) return 0;
-        if(dp[idx][buy][cooldown]!=-1) return dp[idx][buy][cooldown];
-        
-        if(cooldown==1){
-            return dp[idx][buy][cooldown]=func(prices,idx+1,buy,0,dp);
-        }else if(buy==0){
-            int op1 = func(prices,idx+1,0,0,dp);
-            int op2 = -prices[idx]+func(prices,idx+1,1,0,dp);
-            return dp[idx][buy][cooldown]=Math.max(op1,op2);
-        }else{
-            int op1 = func(prices,idx+1,1,0,dp);
-            int op2 = prices[idx]+func(prices,idx+1,0,1,dp);
-            return dp[idx][buy][cooldown]=Math.max(op1,op2);
-        }
+    
+    return dp[0][0];
     }
 }
