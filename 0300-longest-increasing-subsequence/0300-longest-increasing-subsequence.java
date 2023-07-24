09aeb566aa4fa5bb1a7ba280ac917c1d1fded458
Abhishek Rai <63101700+raiabhi007@users.noreply.github.com>
2023-07-24 10:18:22 +0530

    Time: 6 ms (88.88%), Space: 43.9 MB (21.36%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,18 +1,30 @@
 class Solution {
+    public int lowerbound(ArrayList<Integer> list,int key){
+        int low=0;
+        int high = list.size()-1;
+        while(low<high){
+            int mid = (low+high)/2;
+            if(list.get(mid)>=key){
+                high = mid;
+            }else
+                low = mid+1;
+        }
+        return low;
+    }
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
-        int dp[] = new int[n];
-        dp[0] = 1;
-        int ans = 1;
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(nums[0]);
+        int len = 1;
         for(int i=1;i<n;i++){
-            int maxi = 0;
-            for(int j=i-1;j>=0;j--){
-                if(nums[i]>nums[j])
-                maxi = Math.max(maxi,dp[j]);
+            if(nums[i]>list.get(len-1)){
+                list.add(nums[i]);
+                len++;
+            }else{
+                int lb = lowerbound(list,nums[i]);
+                list.set(lb,nums[i]);
             }
-            dp[i]=maxi+1;
-            ans = Math.max(ans,dp[i]);
         }
-        return ans;
+        return len;
     }
 }
