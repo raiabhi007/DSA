d4f5f95f497a252eee5e7b9dc8262985fdf5961e
Abhishek Rai <63101700+raiabhi007@users.noreply.github.com>
2023-07-27 08:09:43 +0530

    Time: 142 ms (30.46%), Space: 42 MB (16.49%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.java
+++ 0312-burst-balloons/0312-burst-balloons.java
@@ -1,34 +1,25 @@
 class Solution {
     public int maxCoins(int[] nums) {
         int n = nums.length;
-        int arr[] = new int[n+2];
+        int[] arr = new int[n+2];
         arr[0] = 1;
-        arr[n+1] = 1;
-        for(int i=1;i<=n;i++)
-            arr[i] = nums[i-1];
         int dp[][] = new int[n+2][n+2];
-        for(int i=n;i>=1;i--){
-            for(int j=1;j<=n;j++){
-                if(i>j) continue;
-                int max = Integer.MIN_VALUE;
-                for(int ind=i;ind<=j;ind++){
-                    int cost = arr[i-1]*arr[ind]*arr[j+1]+dp[i][ind-1]+dp[ind+1][j];
-                    max = Math.max(cost,max);
-                }
-                dp[i][j] = max;
-            }
-        }
-        return dp[1][n];
+        for(int[] row:dp)
+            Arrays.fill(row,-1);
+        for(int i=1;i<=n;i++)
+            arr[i]  = nums[i-1];
+        arr[n+1]=1;
+        return helper(1,n,arr,dp);
     }
-    // public int helper(int i,int j, int[] arr,int[][] dp){
-    //     if(i>j) return 0;
-    //     int max = Integer.MIN_VALUE;
-    //     if(dp[i][j]!=-1)
-    //         return dp[i][j];
-    //     for(int ind=i;ind<=j;ind++){
-    //         int cost = arr[i-1]*arr[ind]*arr[j+1]+helper(i,ind-1,arr,dp)+helper(ind+1,j,arr,dp);
-    //         max = Math.max(cost,max);
-    //     }
-    //     return dp[i][j]=max;
-    // }
+    public int helper(int i,int j,int[] arr,int[][] dp){
+        if(i>j) return 0;
+        if(dp[i][j]!=-1)
+            return dp[i][j];
+        int maxi = Integer.MIN_VALUE;
+        for(int ind=i;ind<=j;ind++){
+            int cost = arr[i-1]*arr[ind]*arr[j+1]+helper(i,ind-1,arr,dp)+helper(ind+1,j,arr,dp);
+            maxi = Math.max(maxi,cost);
+        }
+        return dp[i][j]=maxi;
+    }
 }
