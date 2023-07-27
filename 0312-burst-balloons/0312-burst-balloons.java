bcf5c443f199bd63436887eff8a821acfb38e9de
Abhishek Rai <63101700+raiabhi007@users.noreply.github.com>
2023-07-27 08:16:39 +0530

    Time: 42 ms (86.29%), Space: 41.6 MB (67.62%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.java
+++ 0312-burst-balloons/0312-burst-balloons.java
@@ -3,13 +3,25 @@ class Solution {
         int n = nums.length;
         int[] arr = new int[n+2];
         arr[0] = 1;
-        int dp[][] = new int[n+2][n+2];
-        for(int[] row:dp)
-            Arrays.fill(row,-1);
-        for(int i=1;i<=n;i++)
-            arr[i]  = nums[i-1];
+         for(int i=1;i<=n;i++)
+             arr[i]  = nums[i-1];
         arr[n+1]=1;
-        return helper(1,n,arr,dp);
+        int dp[][] = new int[n+2][n+2];
+        // for(int[] row:dp)
+        //     Arrays.fill(row,-1);   
+        // return helper(1,n,arr,dp);
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=n;j++){
+                if(i>j) continue;
+                int maxi = Integer.MIN_VALUE;
+                for(int ind=i;ind<=j;ind++){
+                    int cost = arr[i-1]*arr[ind]*arr[j+1]+dp[i][ind-1]+dp[ind+1][j];
+                    maxi = Math.max(maxi,cost);
+                }
+                dp[i][j] = maxi;
+            }
+        }
+        return dp[1][n];
     }
     public int helper(int i,int j,int[] arr,int[][] dp){
         if(i>j) return 0;
