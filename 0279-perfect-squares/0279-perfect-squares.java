4e436988e52bc9111bb3e52eafd12e3429f4e959
Abhishek Rai <63101700+raiabhi007@users.noreply.github.com>
2023-07-28 10:05:16 +0530

    Time: 25 ms (92.03%), Space: 42.5 MB (81.90%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -1,9 +1,16 @@
 class Solution {
     public int numSquares(int n) {
-        int dp[] = new int[n+1];
-        Arrays.fill(dp,-1);
-        dp[0] = 0;
-       return helper(n,dp); 
+        int[] dp = new int[n+1];
+        for(int i=1;i<=n;i++){
+            int ans = Integer.MAX_VALUE;
+            int temp = (int)Math.sqrt(i)+1;
+            for(int j=temp;j>=1;j--){
+                if(j*j>i) continue;
+                ans = Math.min(ans,dp[i-j*j]);   
+            }
+            dp[i] = 1+ans;
+        }
+        return dp[n];
     }
     
     public int helper(int n,int[] dp){
