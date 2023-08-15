bd1588b49bcc6b3195ebd103c5156e13a7527278
Abhishek Rai <63101700+raiabhi007@users.noreply.github.com>
2023-08-15 12:17:04 +0530

    Time: 49 ms (97.80%), Space: 42.8 MB (85.38%) - LeetHub

--- 1575-count-all-possible-routes/1575-count-all-possible-routes.java
+++ 1575-count-all-possible-routes/1575-count-all-possible-routes.java
@@ -2,26 +2,25 @@ class Solution {
     int mod = 1000000007;
     public int countRoutes(int[] locations, int start, int finish, int fuel) {
         int n  = locations.length;
-        int dp[][][] = new int[n+1][n+1][fuel+1];
+        int dp[][] = new int[n+1][fuel+1];
         for(int i=0;i<=n;i++){
-            for(int j=0;j<=n;j++){
-                for(int k=0;k<=fuel;k++)
-                    dp[i][j][k] = -1;
+            for(int j=0;j<=fuel;j++){
+                dp[i][j] = -1;
             }
         }
         
         return helper(locations,start,finish,fuel,dp);
     }
-    public int helper(int[] arr,int start,int finish,int fuel,int[][][] dp){
+    public int helper(int[] arr,int start,int finish,int fuel,int[][] dp){
         if(fuel<0) return 0;
         int ans = 0;
-        if(dp[start][finish][fuel]!=-1) return dp[start][finish][fuel];
+        if(dp[start][fuel]!=-1) return dp[start][fuel];
         if(start==finish) ans++;
    
         for(int i=0;i<arr.length;i++){
             if(i==start) continue;
             ans = (ans+helper(arr,i,finish,fuel-Math.abs(arr[start]-arr[i]),dp))%mod;
         }
-        return dp[start][finish][fuel]=ans;
+        return dp[start][fuel]=ans;
     }
 }
