b1c428e7e2743556d4248d509a16fd0310848f29
Abhishek Rai <63101700+raiabhi007@users.noreply.github.com>
2023-08-15 12:07:36 +0530

    Time: 88 ms (38.40%), Space: 56.1 MB (5.10%) - LeetHub

--- 1575-count-all-possible-routes/1575-count-all-possible-routes.java
+++ 1575-count-all-possible-routes/1575-count-all-possible-routes.java
@@ -1,37 +1,27 @@
 class Solution {
-    
-    int MOD = (int)1e9 + 7;
+    int mod = 1000000007;
     public int countRoutes(int[] locations, int start, int finish, int fuel) {
-
-        int l = locations.length;
-        int[][] dp = new int[l + 1][fuel + 1];
-        for(int[] num : dp){
-            Arrays.fill(num, -1);
-        } 
-        return helper(locations, start, finish, fuel, dp);
-    }
-
-    private int helper(int[] locations, int start, int finish, int fuel, int[][] dp) {
-        int result = 0;
-        
-        if(fuel < 0) return result;
-        
-        if(dp[start][fuel] != -1) return dp[start][fuel];
-        
-        // If the start and finish are the same, increment the result
-        if(start == finish) result++;
-        
-        for(int i = 0; i < locations.length; i++) {
-            if(i == start) continue; // Skip the current city if it is the same as the start city
-            
-            int fuelConsumed = Math.abs(locations[start] - locations[i]);
-            
-            // Recursively calculate the count of routes from city i to the finish city
-            result += helper(locations, i, finish, fuel - fuelConsumed, dp);
-            result %= MOD;
+        int n  = locations.length;
+        int dp[][][] = new int[n+1][n+1][fuel+1];
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=n;j++){
+                for(int k=0;k<=fuel;k++)
+                    dp[i][j][k] = -1;
+            }
         }
-        // Store the result in the dp array 
-        dp[start][fuel] = result;
-        return result;
+        
+        return helper(locations,start,finish,fuel,dp);
+    }
+    public int helper(int[] arr,int start,int finish,int fuel,int[][][] dp){
+        if(fuel<0) return 0;
+        int ans = 0;
+        if(dp[start][finish][fuel]!=-1) return dp[start][finish][fuel];
+        if(start==finish) ans++;
+   
+        for(int i=0;i<arr.length;i++){
+            if(i==start) continue;
+            ans = (ans+helper(arr,i,finish,fuel-Math.abs(arr[start]-arr[i]),dp))%mod;
+        }
+        return dp[start][finish][fuel]=ans;
     }
 }
