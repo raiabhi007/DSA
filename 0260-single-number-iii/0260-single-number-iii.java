48de52f9c794f2d45ab45df84b99a5b24c80a5dc
Abhishek Rai <63101700+raiabhi007@users.noreply.github.com>
2023-08-21 08:42:45 +0530

    Time: 1 ms (99.76%), Space: 45 MB (7.12%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.java
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -1,21 +1,28 @@
 class Solution {
     public int[] singleNumber(int[] nums) {
-        int ans[] = new int[2];
-        int temp = nums[0];
-        for(int i=1;i<nums.length;i++){
-            temp = temp^nums[i];
+        int n = nums.length;
+        int sum = 0;
+        for(int i=0;i<n;i++){
+            sum = sum^nums[i];
         }
-        int setBits = (temp& ~(temp-1));
-        int x = 0;
-        int y = 0;
-        for(int i=0;i<nums.length;i++){
-            if((nums[i]&setBits)>0){
-                x = x^nums[i];
-            }else
-                y = y^nums[i];
+        int temp = sum;
+        int pos = 0;
+        while(temp!=0){
+            if((temp&1)>0){
+                break;
+            }
+            pos++;
+            temp = temp>>1;
         }
-        ans[0] = x;
-        ans[1] = y;
+        int filter=0;
+        for(int i=0;i<n;i++){
+            if((nums[i]&(1<<pos))>0){
+                filter = filter^nums[i];
+            }
+        }
+        int[] ans = new int[2];
+        ans[0] = filter;
+        ans[1] = sum^filter;
         return ans;
     }
 }
