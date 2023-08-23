765f399caefee62475131efc83a726fbb80344b6
Abhishek Rai <63101700+raiabhi007@users.noreply.github.com>
2023-08-23 07:51:45 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (61.98%) - LeetHub

--- 1255-maximum-score-words-formed-by-letters/1255-maximum-score-words-formed-by-letters.java
+++ 1255-maximum-score-words-formed-by-letters/1255-maximum-score-words-formed-by-letters.java
@@ -1,26 +1,22 @@
 class Solution {
     public int maxScoreWords(String[] words, char[] letters, int[] score) {
-        HashMap<Character,Integer> map = new HashMap<>();
+        int count[] = new int[26];
         for(int i=0;i<letters.length;i++){
-            if(!map.containsKey(letters[i])){
-                map.put(letters[i],1);
-            }else
-                map.put(letters[i],map.get(letters[i])+1);
+            count[letters[i]-'a']++;
         }
-        return backtrack(0,words,score,map);
+        return backtrack(0,words,score,count);
         
     }
-    private int backtrack(int idx, String[] words,int[] score,HashMap<Character,Integer> map){
+    private int backtrack(int idx, String[] words,int[] score,int[] count){
         if(idx==words.length) return 0;
-        int exc = backtrack(idx+1,words,score,map);
+        int exc = backtrack(idx+1,words,score,count);
          int sum = 0;
          int i=0;
         for(i=0;i<words[idx].length();i++){
             char c = words[idx].charAt(i);
-            if(map.containsKey(c)){
+            if(count[c-'a']>0){
                 sum+=score[c-'a'];
-                map.put(c,map.get(c)-1);
-                if(map.get(c)==0) map.remove(c);
+                count[c-'a']--;
             }else{
                 sum = 0;
                 break;
@@ -28,13 +24,10 @@ class Solution {
         }
         int inc = 0;
         if(sum!=0)
-        inc = sum+backtrack(idx+1,words,score,map);
+        inc = sum+backtrack(idx+1,words,score,count);
         for(int j=0;j<i;j++){
             char c = words[idx].charAt(j);
-            if(!map.containsKey(c)){
-                map.put(c,1);
-            }else
-                map.put(c,map.get(c)+1);
+            count[c-'a']++;
         }
         return Math.max(inc,exc);
     }
