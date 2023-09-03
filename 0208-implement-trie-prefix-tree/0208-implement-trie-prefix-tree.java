6384119abb6327e4e61a7149db61b0547add063b
Abhishek Rai <63101700+raiabhi007@users.noreply.github.com>
2023-09-03 17:36:03 +0530

    Time: 37 ms (68.48%), Space: 54.6 MB (67.56%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.java
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.java
@@ -1,17 +1,17 @@
-class Node{
-    Node links[] = new Node[26];
+class Node {
+    Node[] links = new Node[26];
     boolean flag = false;
     public Node(){
 
     }
     boolean containsKey(char ch){
-        return links[ch-'a']!=null;
+        return (links[ch-'a']!=null);
     }
     Node get(char ch){
         return links[ch-'a'];
     }
     void put(char ch, Node node){
-        links[ch-'a']=node;
+        links[ch-'a'] = node;
     }
     void setEnd(){
         flag = true;
@@ -20,6 +20,7 @@ class Node{
         return flag;
     }
 }
+
 class Trie {
     private Node root;
     public Trie() {
@@ -29,9 +30,8 @@ class Trie {
     public void insert(String word) {
         Node node = root;
         for(int i=0;i<word.length();i++){
-            if(!node.containsKey(word.charAt(i))){
+            if(!node.containsKey(word.charAt(i)))
                 node.put(word.charAt(i),new Node());
-            }
             node = node.get(word.charAt(i));
         }
         node.setEnd();
@@ -44,9 +44,7 @@ class Trie {
                 return false;
             node = node.get(word.charAt(i));
         }
-        if(node.isEnd())
-            return true;
-        return false;
+        return node.isEnd();
     }
     
     public boolean startsWith(String prefix) {
