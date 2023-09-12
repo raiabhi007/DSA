c534d19af0386c95b7f8202fd80cfd471e8a5745
Abhishek Rai <63101700+raiabhi007@users.noreply.github.com>
2023-09-12 08:03:01 +0530

    Time: 16 ms (18.99%), Space: 43.5 MB (35.95%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.java
+++ 1402-reducing-dishes/1402-reducing-dishes.java
@@ -1,17 +1,19 @@
 class Solution {
-public int maxSatisfaction(int[] satisfaction) {
-Arrays.sort(satisfaction);
-
-    int maxVlaue=0;
-    for(int i=0;i<satisfaction.length;i++){
-        int cook=1;
-        int sum=0;
-        for(int j=i;j<satisfaction.length;j++){
-        sum+=satisfaction[j]*cook;
-        cook++;
-        }
-        maxVlaue=Math.max(sum,maxVlaue);
+    public int maxSatisfaction(int[] satisfaction) {
+        int n = satisfaction.length;
+        int dp[][] = new int[n][n+1];
+        for(int i=0;i<n;i++)
+            Arrays.fill(dp[i],-1);
+        Arrays.sort(satisfaction);
+        return helper(satisfaction,0,1,dp);
+        
     }
-    return  maxVlaue;
-}
+    public int helper(int[] satisfaction,int i,int time,int[][] dp){
+        if(i==satisfaction.length) return 0;
+        if(dp[i][time]!=-1) return dp[i][time];
+        
+        int inc = time*satisfaction[i] + helper(satisfaction,i+1,time+1,dp);
+        int exc = helper(satisfaction,i+1,time,dp);
+        return dp[i][time]=Math.max(inc,exc);
+    }
 }
