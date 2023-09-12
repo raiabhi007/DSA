6ac16b1184c1aa523e9ee11195ec438ff0de2d67
Abhishek Rai <63101700+raiabhi007@users.noreply.github.com>
2023-09-12 08:23:51 +0530

    Time: 2 ms (93.04%), Space: 40.3 MB (88.73%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.java
+++ 1402-reducing-dishes/1402-reducing-dishes.java
@@ -1,26 +1,11 @@
 class Solution {
-    public int maxSatisfaction(int[] satisfaction) {
-        int n = satisfaction.length;
-        int dp[][] = new int[n+1][n+2];
-        Arrays.sort(satisfaction);
-        for(int i=n-1;i>=0;i--){
-            for(int time=n;time>=1;time--){
-                if(i==n-1) dp[i][time]=0;
-                int inc = time*satisfaction[i]+ dp[i+1][time+1];
-                int exc = dp[i+1][time];
-                dp[i][time] = Math.max(inc,exc);
-            }
+    public int maxSatisfaction(int[] A) {
+        Arrays.sort(A);
+        int res = 0, total = 0, n = A.length;
+        for (int i = n - 1; i >= 0 && A[i] > -total; --i) {
+            total += A[i];
+            res += total;
         }
-        
-        return dp[0][1];
-        
-    }
-    public int helper(int[] satisfaction,int i,int time,int[][] dp){
-        if(i==satisfaction.length) return 0;
-        if(dp[i][time]!=-1) return dp[i][time];
-        
-        int inc = time*satisfaction[i] + helper(satisfaction,i+1,time+1,dp);
-        int exc = helper(satisfaction,i+1,time,dp);
-        return dp[i][time]=Math.max(inc,exc);
+        return res;
     }
 }
