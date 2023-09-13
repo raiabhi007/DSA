6e6a18dfaebae93fc304c3810d05596f2337ad2f
Abhishek Rai <63101700+raiabhi007@users.noreply.github.com>
2023-09-13 14:20:25 +0530

    Time: 7 ms (44.71%), Space: 43.1 MB (51.15%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -1,31 +1,29 @@
 class Solution {
     public int maxProfit(int k, int[] prices) {
-         int[][][] dp = new int[prices.length+1][2][k+1];
-        for(int i=prices.length-1;i>=0;i--){
-            for(int buy=0;buy<=1;buy++){
-                for(int cap=1;cap<=k;cap++){
-                    if(buy==0)
-                        dp[i][0][cap] = Math.max(dp[i+1][0][cap],-prices[i]+dp[i+1][1][cap]);
-                    else
-                        dp[i][1][cap] = Math.max(dp[i+1][1][cap],prices[i]+dp[i+1][0][cap-1]);
-                }
+        int n = prices.length;
+        int dp[][][] = new int[n][2][k+1];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<2;j++){
+                for(int l=0;l<=k;l++)
+                    dp[i][j][l] = -1;
             }
         }
-        return dp[0][0][k];
+        return helper(prices,0,0,k,dp);
     }
-    public int func(int[] prices,int idx,int buy,int k,int[][][] dp){
-        if(k==0) return 0;
+    public int helper(int[] prices,int idx,int buy,int k,int[][][] dp){
         if(idx==prices.length) return 0;
-        if(dp[idx][buy][k]!=-1)
-            return dp[idx][buy][k];
+        if(k==0) return 0;
+        if(dp[idx][buy][k]!=-1) return dp[idx][buy][k];
+        int profit=0;
         if(buy==0){
-            int op1 = func(prices,idx+1,0,k,dp);
-            int op2 = -prices[idx]+func(prices,idx+1,1,k,dp);
-            return dp[idx][buy][k]=Math.max(op1,op2);
+            int temp1 = helper(prices,idx+1,0,k,dp);
+            int temp2 = -prices[idx]+helper(prices,idx+1,1,k,dp);
+            profit = Math.max(temp1,temp2);
         }else{
-            int op1 = func(prices,idx+1,1,k,dp);
-            int op2 = prices[idx]+func(prices,idx+1,0,k-1,dp);
-            return dp[idx][buy][k]=Math.max(op1,op2);
+            int temp1 = helper(prices,idx+1,1,k,dp);
+            int temp2 = prices[idx]+helper(prices,idx+1,0,k-1,dp);
+            profit = Math.max(temp1,temp2);
         }
+        return dp[idx][buy][k]=profit;
     }
 }
