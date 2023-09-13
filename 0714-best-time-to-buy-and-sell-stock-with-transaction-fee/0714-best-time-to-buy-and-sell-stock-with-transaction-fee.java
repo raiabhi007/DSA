1b72af1ef307f51b5bf7f87ecbc4832b650b57e1
Abhishek Rai <63101700+raiabhi007@users.noreply.github.com>
2023-09-13 14:33:42 +0530

    Time: 29 ms (23.43%), Space: 64.3 MB (13.60%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -1,28 +1,28 @@
 class Solution {
     public int maxProfit(int[] prices, int fee) {
-        int dp[][] = new int[prices.length+1][2];
-        for(int i=prices.length-1;i>=0;i--){
-           for(int buy=0;buy<=1;buy++){
-               if(buy==0)
-                   dp[i][buy] = Math.max(dp[i+1][0],-prices[i]-fee+dp[i+1][1]);
-               else
-                   dp[i][buy] = Math.max(dp[i+1][1],prices[i]+dp[i+1][0]);
-           }
+         int dp[][] = new int[prices.length][2];
+        for(int i=0;i<prices.length;i++){
+            dp[i][0]=-1;
+            dp[i][1]=-1;
         }
-        return dp[0][0];
-    }
-    public int func(int[] prices,int idx,int buy,int fee,int[][] dp){
-        if(idx==prices.length) return 0;
-        if(dp[idx][buy]!=-1) return dp[idx][buy];
         
+        return helper(prices,0,0,fee,dp);
+    }
+     public int helper(int prices[],int i,int buy,int fee,int[][]dp){
+        if(i==prices.length) return 0;
+        if(dp[i][buy]!=-1) return dp[i][buy];
+        int profit=0;
         if(buy==0){
-            int op1 = func(prices,idx+1,0,fee,dp);
-            int op2 = -prices[idx]-fee+func(prices,idx+1,1,fee,dp);
-            return dp[idx][buy]=Math.max(op1,op2);
+            int temp1 = helper(prices,i+1,0,fee,dp);
+            int temp2 = -prices[i]+helper(prices,i+1,1,fee,dp);
+             profit=Math.max(temp1,temp2);
         }else{
-            int op1 = func(prices,idx+1,1,fee,dp);
-            int op2 = prices[idx]+func(prices,idx+1,0,fee,dp);
-            return dp[idx][buy]=Math.max(op1,op2);
+            int temp1 = helper(prices,i+1,1,fee,dp);
+            int temp2 = prices[i]+helper(prices,i+1,0,fee,dp)-fee;
+            profit=Math.max(temp1,temp2);
         }
+        dp[i][buy] = profit;
+        return profit;
+        
     }
 }
