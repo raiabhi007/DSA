e005475c9a8f66b2866a4daf11a49574126f7db4
Abhishek Rai <63101700+raiabhi007@users.noreply.github.com>
2023-09-15 08:42:18 +0530

    Time: 28 ms (18.76%), Space: 40.9 MB (50.67%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
@@ -2,9 +2,15 @@ class Solution {
     int mod = 1000000007;
     public int numRollsToTarget(int n, int k, int target) {
         int dp[][] = new int[n+1][target+1];
-        for(int i=0;i<=n;i++)
-        Arrays.fill(dp[i],-1);
-        return helper(n,k,target,dp);
+        dp[0][0] = 1;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=target;j++){
+                for(int ways=1;ways<=k;ways++)
+                    if(j>=ways)
+                    dp[i][j] = (dp[i][j] + dp[i-1][j-ways]%mod)%mod;
+            }
+        }
+        return dp[n][target];
     }
     public int helper(int n,int k,int target,int[][]dp){
         if(target==0&&n==0) return 1;
