4ca4be43026c9aa7484c48a9a1f09d70090f0312
Abhishek Rai <63101700+raiabhi007@users.noreply.github.com>
2024-02-13 00:55:48 +0530

    Time: 35 ms (40.20%), Space: 58.1 MB (87.60%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -1,16 +1,17 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        int[] ans = new int[nums.length-k+1];
+        int n = nums.length;
+        int ans[] = new int[n-k+1];
         int j=0;
-        Deque<Integer> q = new LinkedList<>();
-        for(int i=0;i<nums.length;i++){
-            if (!q.isEmpty() && q.peekFirst()<i-k+1) 
-            q.pollFirst();
-            while (!q.isEmpty() && nums[i] > nums[q.peekLast()]) 
-            q.pollLast();
-            q.offer(i);
-            if (i >= k - 1) 
-            ans[j++] = nums[q.peekFirst()];
+        Deque<Integer> dq = new LinkedList<>();
+        for(int i=0;i<n;i++){
+            if(!dq.isEmpty()&&dq.peekFirst()<i-k+1)
+                dq.pollFirst();
+            while(!dq.isEmpty()&&nums[i]>nums[dq.peekLast()])
+                dq.pollLast();
+            dq.offer(i);
+            if(i>=k-1)
+                ans[j++] = nums[dq.peekFirst()];
         }
         return ans;
     }
