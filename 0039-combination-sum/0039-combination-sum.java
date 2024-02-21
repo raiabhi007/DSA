880db80192352b8ae51fd6bc281a78d981b95c28
Abhishek Rai <63101700+raiabhi007@users.noreply.github.com>
2024-02-21 23:33:12 +0530

    Time: 2 ms (86.13%), Space: 44.3 MB (92.21%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,24 +1,25 @@
 class Solution {
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+    public List<List<Integer>> combinationSum(int[] candidates, int target){
         List<List<Integer>> ans = new ArrayList<>();
         List<Integer> list = new ArrayList<>();
-        backtrack(candidates,0,0,target,list,ans);
+        helper(candidates,0,target,ans,list);
         return ans;
     }
-    void backtrack(int[] arr,int i,int sum,int tar,List<Integer> list,List<List<Integer>> ans){
-        if(i==arr.length){
-            if(sum==tar){
-                ans.add(new ArrayList<>(list));
-            }
+    public void helper(int[] arr,int idx,int target,List<List<Integer>> ans,List<Integer> list){
+        if(target==0){
+            ans.add(new ArrayList<>(list));
             return;
         }
-        for(int j=(tar-sum)/arr[i];j>=1;j--){
-            for(int k=0;k<j;k++)
-                list.add(arr[i]);
-            backtrack(arr,i+1,sum+arr[i]*j,tar,list,ans);
-            for(int k=0;k<j;k++)
-                list.remove(list.size()-1);
+        
+        if(arr.length==idx) return;
+        int itr=0;
+        for(int sum=arr[idx];sum<=target;sum+=arr[idx]){
+            list.add(arr[idx]);
+            helper(arr,idx+1,target-sum,ans,list);
+            itr++;
         }
-        backtrack(arr,i+1,sum,tar,list,ans);
+        while(itr-->0)
+            list.remove(list.size()-1);
+        helper(arr,idx+1,target,ans,list);
     }
 }
