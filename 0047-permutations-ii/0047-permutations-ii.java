126d87a4ca524b9ca1e8cf4b54ebd846b11d2aec
Abhishek Rai <63101700+raiabhi007@users.noreply.github.com>
2024-02-26 20:49:14 +0530

    Time: 2 ms (82.98%), Space: 45.3 MB (18.25%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.java
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -1,33 +1,29 @@
 class Solution {
     public List<List<Integer>> permuteUnique(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
-        backtrack(ans,nums,0);
+        helper(nums,0,ans);
         return ans;
     }
-    
-    public void backtrack(List<List<Integer>> ans,int[] nums,int start){
-        if(start==nums.length){
+    public void helper(int nums[],int idx,List<List<Integer>> ans){
+        if(idx==nums.length){
             ans.add(toList(nums));
             return;
         }
         HashSet<Integer> set = new HashSet<>();
-        for(int i=start;i<nums.length;i++){
-            if(set.contains(nums[i]))
-                continue;
+        for(int i=idx;i<nums.length;i++){
+            if(set.contains(nums[i])) continue;
             set.add(nums[i]);
-            swap(i,start,nums);
-            backtrack(ans,nums,start+1);
-            swap(i,start,nums);
+            swap(i,idx,nums);
+            helper(nums,idx+1,ans);
+            swap(i,idx,nums);
         }
     }
-    
-     public void swap(int i,int j,int[] nums){
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
+    public void swap(int i,int j,int[] arr){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
-    
-    public List<Integer> toList(int[] nums){
+     public List<Integer> toList(int[] nums){
         List<Integer> list = new ArrayList<>();
         for(int i:nums)
             list.add(i);
